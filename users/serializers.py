87b54a42044e916e2810d0174c6995f233e35f4e
sswrk <swierk.szymon99@gmail.com>
2021-03-25 21:56:02 +0100

    CHA8 added username to JWT payload

---
+++ users/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super(CustomTokenObtainPairSerializer, cls).get_token(user)
+
+        token['username'] = user.username
+        return token
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+from .views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
 app_name = 'users'
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
