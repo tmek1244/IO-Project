842353bd8bb5b2b819c925a1e1a89fb48567b533
sswrk <swierk.szymon99@gmail.com>
2021-03-25 22:46:40 +0100

    CHA8 added logout

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, LogoutView, LogoutAllView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
 app_name = 'users'
 
 urlpatterns = [
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all')
 ]
