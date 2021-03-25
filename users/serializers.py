83073035190b6f844f7dcd02d32381fbac342e32
sswrk <swierk.szymon99@gmail.com>
2021-03-25 22:22:27 +0100

    CHA8 added user info to JWT obtain request response

--- users/serializers.py
+++ users/serializers.py
@@ -3,11 +3,25 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        data['is_active'] = self.user.is_active
+        data['is_staff'] = self.user.is_staff
+        data['is_superuser'] = self.user.is_superuser
+        return data
+
     @classmethod
     def get_token(cls, user):
         token = super(CustomTokenObtainPairSerializer, cls).get_token(user)
 
         token['username'] = user.username
+        token['email'] = user.email
         token['is_active'] = user.is_active
         token['is_staff'] = user.is_staff
         token['is_superuser'] = user.is_superuser
