f4878eba2c3464af829569960703a0402d0bbf67
sswrk <swierk.szymon99@gmail.com>
2021-03-26 20:01:55 +0100

    CHA8 applied isort and flake8

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView, LogoutView, LogoutAllView
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import CustomTokenObtainPairView, LogoutAllView, LogoutView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', CustomTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
     path('logout_all/', LogoutAllView.as_view(), name='auth_logout_all')
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
+from rest_framework.views import APIView
+from rest_framework_simplejwt.token_blacklist.models import (BlacklistedToken,
+                                                             OutstandingToken)
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from .serializers import CustomTokenObtainPairSerializer
 
 
@@ -23,7 +25,7 @@ class LogoutView(APIView):
             token.blacklist()
 
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
+        except Exception:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
