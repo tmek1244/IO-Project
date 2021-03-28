f9f41085d94979efe18f2d70b57c1692eadd35b6
Michał Lampert <michlampert@pop-os.localdomain>
2021-03-28 07:55:44 +0200

    CHA-2 repair code style

--- register/serializers.py
+++ register/serializers.py
@@ -1,17 +1,17 @@
-from typing import Dict
+from typing import Any, Dict
 
 from django.contrib.auth import get_user_model
 from django.db.models.base import Model
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer[Any]):
     class Meta:
         model = get_user_model()
         fields = ('id', 'first_name', 'last_name', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data: Dict) -> Model:
+    def create(self, validated_data: Dict[Any, Any]) -> Model:
         user = get_user_model().objects.create(
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
