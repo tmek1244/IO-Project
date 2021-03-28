7e52295021b50a61e7e30cf0e0dfedb029d90274
sswrk <swierk.szymon99@gmail.com>
2021-03-28 21:19:39 +0200

    CHA-3 implemented password change endpoint

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from rest_framework import status
+from typing import List
+
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -8,7 +10,8 @@ from rest_framework_simplejwt.token_blacklist.models import (BlacklistedToken,
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import CustomTokenObtainPairSerializer
+from .serializers import (ChangePasswordSerializer,
+                          CustomTokenObtainPairSerializer)
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -39,3 +42,22 @@ class LogoutAllView(APIView):
             t, _ = BlacklistedToken.objects.get_or_create(token=token)
 
         return Response(status=status.HTTP_205_RESET_CONTENT)
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
+
+    def update(self, request: Request,
+               *args: List[str], **kwargs: List[str]) -> Response:
+        user = self.request.user
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not user.check_password(serializer.data.get("old_password")):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.data.get("new_password"))
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
