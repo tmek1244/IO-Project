128d61c9693dcf3840de59ba915795a3552accd8
Michał Lampert <michlampert@pop-os.localdomain>
2021-03-28 07:08:38 +0200

    CHA-2 add registration view

--- register/views.py
+++ register/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAdminUser
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class CreateUserView(CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+
