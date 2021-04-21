07fa5fa9b5b4cfac89ee349201840b35d3fada5a
michlampert <michlampert@gmail.com>
2021-04-21 04:20:16 +0200

    CHA-34 make changes in data generator

--- generate_data.py
+++ generate_data.py
@@ -21,14 +21,14 @@ class Candidate:
     def __init__(self) -> None:
         self.first_name = fake.first_name()
         self.last_name = fake.last_name()
-        self.data_of_birth = fake.date_between(
+        self.date_of_birth = fake.date_between(
             start_date='-30y', end_date='-19y')
-        self.gender = random.choice(['kobieta', 'mezczyzna'])
-        sth = datetime.datetime.now().year - self.data_of_birth.year - 18
+        self.gender = random.choice(['K', 'M', 'M', ""])
+        sth = datetime.datetime.now().year - self.date_of_birth.year - 18
         self.year_of_exam = fake.date_between(
             start_date=f'-{sth}y '
         ).year
-        self.city = fake.city()
+        self.city = GraduatedSchool().city
 
     def __str__(self) -> str:
         return convert_to_str(vars(self))
@@ -47,56 +47,105 @@ class Candidate:
 #         self.mode = 'null'
 
 
-HIGH_SCHOOL = [
-    {'school_city': 'Katowice', 'school_type': 'T',
-     'school_name': 'STZN', 'faculty': '', 'field_of_study': '',
-     'mode': 'stacjonarne'},
-    {'school_city': 'Katowice', 'school_type': 'L',
-     'school_name': 'VIII LO', 'faculty': '', 'field_of_study': '',
-     'mode': 'stacjonarne'},
-    {'school_city': 'Krakow', 'school_type': 'L',
-     'school_name': 'V LO', 'faculty': '', 'field_of_study': '',
-     'mode': 'stacjonarne'},
-]
+class FieldOfStudy:
+    fofs = [
+        ("WIET", "Informatyka"),
+        ("WIET", "Elektronika"),
+        ("WIET", "Telekomunikacja"),
+        ("WMS", "Matematyka"),
+        ("WIMIR", "Automatyka"),
+        ("WIMIR", "Robotyka"),
+        ("FIS", "Fizyka"),
+        ("FIS", "Informatyka"),
+        ("Zarządzania", "Zarządzanie"),
+        ("Ogólny", "Górnicwo"),
+        ("Ogólny", "Hutnictwo"),
+    ]
 
-UNIVERSITY = [
-    {'school_city': 'Krakow', 'school_type': 'S1I',
-     'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
-     'mode': 'zaoczne'},
-    {'school_city': 'Krakow', 'school_type': 'S1I',
-     'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
-     'mode': 'stacjonarne'},
-    {'school_city': 'Krakow', 'school_type': 'S2M',
-     'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
-     'mode': 'zaoczne'},
-    {'school_city': 'Krakow', 'school_type': 'S2M',
-     'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
-     'mode': 'stacjonarne'},
+    def __init__(self):
+        self.faculty_name, self.fof_name = random.choice(FieldOfStudy.fofs)
+        self.degree = random.choice(["1"]*4 + ["2"])
+        self.mode = random.choice(["stacjonarne"]*4 + ["niestacjonarne"])
 
-    {'school_city': 'Krakow', 'school_type': 'S1I',
-     'school_name': 'PK', 'faculty': 'WSI', 'field_of_study': 'Elektronika',
-     'mode': 'stacjonarne'},
-    {'school_city': 'Krakow', 'school_type': 'S2M',
-     'school_name': 'PK', 'faculty': 'WSI', 'field_of_study': 'Elektronika',
-     'mode': 'stacjonarne'},
+class GraduatedSchool:
+    # [city, name, type, date, faculty, fof]
+    hss = [
+        ("Kraków", "V", "L", "", "", ""),
+        ("Kraków", "III", "L", "", "", ""),
+        ("Kraków", "X", "L", "", "", ""),
+        ("Warszawa", "III", "L", "", "", ""),
+        ("Warszawa", "I", "L", "", "", ""),
+        ("Warszawa", "V", "L", "", "", ""),
+        ("Katowice", "VIII", "L", "", "", ""),
+        ("Katowice", "III", "L", "", "", ""),
+        ("Gdańsk", "I", "L", "", "", ""),
+        ("Wrocław", "VI", "L", "", "", ""),
+        ("Kraków", "I", "T", "", "", ""),
+    ]
 
-    {'school_city': 'Gdansk', 'school_type': 'S1L',
-     'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
-     'field_of_study': 'Odlewnictwo',
-     'mode': 'zaoczne'},
-    {'school_city': 'Gdansk', 'school_type': 'S1L',
-     'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
-     'field_of_study': 'Odlewnictwo',
-     'mode': 'stacjonarne'},
-    {'school_city': 'Gdansk', 'school_type': 'S2M',
-     'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
-     'field_of_study': 'Odlewnictwo',
-     'mode': 'zaoczne'},
-    {'school_city': 'Gdansk', 'school_type': 'S2M',
-     'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
-     'field_of_study': 'Odlewnictwo',
-     'mode': 'stacjonarne'}
-]
+    universities = [("Kraków", "AGH", "B", "") + fof for fof in FieldOfStudy.fofs] + [
+        ("Kraków", "UJ", "B", "", "TCS", "TCS"),
+        ("Kraków", "UJ", "B", "", "Filozoficzny", "Filozofia"),
+        ("Kraków", "PK", "B", "", "Ogólny", "Informatyka"),
+        ("Kraków", "PK", "B", "", "Ogólny", "Robotyka"),
+        ("Kraków", "PK", "B", "", "Ogólny", "Budownictwo"),
+        ("Wrocław", "PW", "B", "", "Ogólny", "Informatyka"),
+        ("Warszawa", "UW", "B", "", "Ogólny", "Informatyka"),
+        ("Warszawa", "PW", "B", "", "Ogólny", "Informatyka"),
+    ]
+    def __init__(self):
+        self.city, self.name, self.degree, self.date, self.faculty, self.fof = random.choice(GraduatedSchool.hss*4 + GraduatedSchool.universities)
+
+# HIGH_SCHOOL = [
+#     {'school_city': 'Katowice', 'school_type': 'T',
+#      'school_name': 'STZN', 'faculty': '', 'field_of_study': '',
+#      'mode': 'stacjonarne'},
+#     {'school_city': 'Katowice', 'school_type': 'L',
+#      'school_name': 'VIII LO', 'faculty': '', 'field_of_study': '',
+#      'mode': 'stacjonarne'},
+#     {'school_city': 'Krakow', 'school_type': 'L',
+#      'school_name': 'V LO', 'faculty': '', 'field_of_study': '',
+#      'mode': 'stacjonarne'},
+# ]
+
+# UNIVERSITY = [
+#     {'school_city': 'Krakow', 'school_type': 'S1I',
+#      'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
+#      'mode': 'zaoczne'},
+#     {'school_city': 'Krakow', 'school_type': 'S1I',
+#      'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
+#      'mode': 'stacjonarne'},
+#     {'school_city': 'Krakow', 'school_type': 'S2M',
+#      'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
+#      'mode': 'zaoczne'},
+#     {'school_city': 'Krakow', 'school_type': 'S2M',
+#      'school_name': 'AGH', 'faculty': 'WIET', 'field_of_study': 'Informatyka',
+#      'mode': 'stacjonarne'},
+
+#     {'school_city': 'Krakow', 'school_type': 'S1I',
+#      'school_name': 'PK', 'faculty': 'WSI', 'field_of_study': 'Elektronika',
+#      'mode': 'stacjonarne'},
+#     {'school_city': 'Krakow', 'school_type': 'S2M',
+#      'school_name': 'PK', 'faculty': 'WSI', 'field_of_study': 'Elektronika',
+#      'mode': 'stacjonarne'},
+
+#     {'school_city': 'Gdansk', 'school_type': 'S1L',
+#      'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
+#      'field_of_study': 'Odlewnictwo',
+#      'mode': 'zaoczne'},
+#     {'school_city': 'Gdansk', 'school_type': 'S1L',
+#      'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
+#      'field_of_study': 'Odlewnictwo',
+#      'mode': 'stacjonarne'},
+#     {'school_city': 'Gdansk', 'school_type': 'S2M',
+#      'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
+#      'field_of_study': 'Odlewnictwo',
+#      'mode': 'zaoczne'},
+#     {'school_city': 'Gdansk', 'school_type': 'S2M',
+#      'school_name': 'Uczelnia w Gadansku', 'faculty': 'WCI',
+#      'field_of_study': 'Odlewnictwo',
+#      'mode': 'stacjonarne'}
+# ]
 
 
 # class University:
@@ -110,54 +159,85 @@ UNIVERSITY = [
 #         self.mode = random.choice(["zaoczne", "stacjonarne"])
 
 
-class Grade:
-    def __init__(self) -> None:
-        self.IT = random.randint(2, 6)
-        self.math = random.randint(2, 6)
-        self.english = random.randint(2, 6)
+# class Grade:
+#     def __init__(self) -> None:
+#         self.IT = random.randint(2, 6)
+#         self.math = random.randint(2, 6)
+#         self.english = random.randint(2, 6)
 
 
-class ExamResult:
-    def __init__(self) -> None:
-        self.IT = random.randint(30, 100)
-        self.math = random.randint(30, 100)
-        self.english = random.randint(30, 100)
+# class ExamResult:
+#     def __init__(self) -> None:
+#         self.IT = random.randint(30, 100)
+#         self.math = random.randint(30, 100)
+#         self.english = random.randint(30, 100)
 
 
 class Recruitment:
-    def __init__(self, round_nr: Any) -> None:
-        self.year = random.randint(2016, 2021)
-        self.round = round_nr
-        self.faculty, self.field_of_study = random.choice([
-            ("WIET", "Informatyka"),
-            ("WIET", "Elektronika"),
-            ("WIMIR", "Odlewnictwo")
-        ])
-        self.points = random.randint(100, 1000)
+    def __init__(self) -> None:
+        self.year = random.randint(2018, 2020)
+        self.round = random.randint(1,3)
+        self.field_of_study = FieldOfStudy()
+        self.points = random.randint(800, 1000) if self.field_of_study.fof_name=="Informatyka" else random.randint(100,1000)
+        self.olympiad = random.choice(["Diament"] *3 + ["OM"] + [""] * 20 )
         self.result = random.choice(
-            ["Accepted", "Rejected", "Signed"])
+            [
+                "nieprzyjęty z powodu niedokonania wpisu",
+                "niezakwalifikowany",
+                "rekrutacja zakończona",
+                "wpisany",
+                "rekrutacja zakończona",
+                "wpisany",
+            ]
+        )
 
+
+# def main(persons: Any, file_name: Any) -> Any:
+#     with open(file_name, 'w') as file:
+#         record = [vars(Candidate()), random.choice(HIGH_SCHOOL),
+#                   vars(Grade()), vars(ExamResult()), vars(Recruitment(1))]
+#         dictionary = []
+#         for rec in record:
+#             for x in rec.keys():
+#                 dictionary.append(x)
+
+#         print(','.join(dictionary))
+#         for i in range(persons):
+#             record = [vars(Candidate()), random.choice(HIGH_SCHOOL),
+#                       vars(Grade()), vars(ExamResult())]
+#             for nr in range(1, 6):
+#                 if random.random() > 0.8:
+#                     break
+#                 print(
+#                     ','.join(convert_to_str(x) for x in record +
+#                              [vars(Recruitment(nr))]), file=file)
 
 def main(persons: Any, file_name: Any) -> Any:
-    with open(file_name, 'w') as file:
-        record = [vars(Candidate()), random.choice(HIGH_SCHOOL),
-                  vars(Grade()), vars(ExamResult()), vars(Recruitment(1))]
-        dictionary = []
-        for rec in record:
-            for x in rec.keys():
-                dictionary.append(x)
+    f = open(file_name, "w")
+    print(
+        "no", "rok", "runda",
+        "rodzaj", "stopień", "wydział", "kierunek",
+        "status", "punkty", "olimpiada", "data_aplikacji",
+        "nazwisko", "imię", "imię2", "imię_ojca", "imię_matki", "pesel", "płeć", 
+        "ulica", "nr_domu", "nr_mieszkania", "miasto", "kod_pocztowy", "poczta", "kraj", "email",
+        "szkoła_kraj", "szkoła_miasto", "szkoła_nazwa", "szkoła_data", "szkoła_stopień", "szkoła_wydział", "szkoła_kierunek", 
+        sep=","
+    )
+    for i in range(persons):
+        c = Candidate()
+        r = Recruitment()
+        fof = r.field_of_study
+        gs = GraduatedSchool()
 
-        print(','.join(dictionary))
-        for i in range(persons):
-            record = [vars(Candidate()), random.choice(HIGH_SCHOOL),
-                      vars(Grade()), vars(ExamResult())]
-            for nr in range(1, 6):
-                if random.random() > 0.8:
-                    break
-                print(
-                    ','.join(convert_to_str(x) for x in record +
-                             [vars(Recruitment(nr))]), file=file)
+        print(random.randint(1000,10000), r.year, r.round, # ogólne
+              fof.mode, fof.degree, fof.faculty_name, fof.fof_name, # wydział gdzie aplikował
+              r.result, r.points, r.olympiad, "", # z jakim wynikiem
+              c.last_name, c.first_name, "", "", "", random.randint(10**10, 10**11), c.gender, # kto aplikował
+              "","","", c.city, "", "", "PL", "", # skąd jest
+              "PL", gs.city, gs.name, gs.date, gs.degree, gs.faculty, gs.fof, # z jakiej poprzedniej szkoły
+              sep=",", file=f
+        )
 
 
 if __name__ == '__main__':
-    main(persons=10, file_name='generated_data.csv')
+    main(persons=100, file_name='new_generated_data.csv')
