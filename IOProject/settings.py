25879f47c8573a014c6c6d4aa9e60046e4f541a6
tmek1244 <tmek1244@wp.pl>
2021-05-01 18:51:27 +0200

    CHA-63 Added swagger

--- IOProject/settings.py
+++ IOProject/settings.py
@@ -18,7 +18,6 @@ from typing import List
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -46,6 +45,8 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'backend',
     'users',
+    # required for serving swagger ui's css/js files
+    'drf_yasg',
 ]
 AUTH_USER_MODEL = "users.EmailUser"
 
@@ -79,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'IOProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -94,7 +94,6 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', 5433),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -133,7 +132,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
 }
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -146,7 +144,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
