d424c48e7895ed3fef6c34870ab2649025d2cf78
michlampert <michlampert@gmail.com>
2021-05-10 01:45:06 +0200

    Create basic view

--- backend/views.py
+++ backend/views.py
@@ -293,3 +293,66 @@ class CompareFields(APIView):
         except Exception as e:
             print(e)
             return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+
+class LaureatesOnFOFSView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request: Request, year: int,
+            faculty: str = None) -> Response:
+        try:
+            if faculty:
+                tmp = list(RecruitmentResult.objects.
+                           filter(recruitment__year=year).
+                           filter(recruitment__result__in=["+", "accepted", "signed"]).
+                           filter(recruitment__student__contest__not_in=[None, ""]).
+                           filter(
+                             recruitment__field_of_study__faculty__name=faculty
+                           ).
+                           filter(
+                             recruitment__field_of_study__degree__not_in=["2","3","4"]
+                           ).
+                           values(
+                               'recruitment__field_of_study__name')
+                           .annotate(total=Count('recruitment__field_of_study__name')).
+                           order_by('total'))
+            # else:
+            #     tmp = list(RecruitmentResult.objects.
+            #                filter(recruitment__year=year).
+            #                values(
+            #                    'recruitment__field_of_study__name',
+            #                    'recruitment__round',
+            #                    'result')
+            #                .annotate(total=Count('result')).
+            #                order_by('total'))
+
+            result: Dict[Any, Any] = {"all": {}}
+
+            print(tmp)
+            
+            # for d in tmp:
+            #     fof = d['recruitment__field_of_study__name']
+            #     round = d['recruitment__round']
+            #     rstatus = d['result']
+            #     total = d['total']
+
+            #     if fof not in result:
+            #         result[fof] = {"all": {}}
+            #     if round not in result[fof]:
+            #         result[fof][round] = {}
+            #     if rstatus not in result["all"]:
+            #         result["all"][rstatus] = 0
+            #     if rstatus not in result[fof]["all"]:
+            #         result[fof]["all"][rstatus] = 0
+
+            #     result[fof][round][rstatus] = total
+            #     result["all"][rstatus] += total
+            #     result[fof]["all"][rstatus] += total
+
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+            return Response(
+                {"problem": str(e)},
+                status=status.HTTP_503_SERVICE_UNAVAILABLE
+            )
