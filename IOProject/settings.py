fb320961d40f0b59938d3582ebdf329a3966535a
tmek1244 <tmek1244@wp.pl>
2021-05-28 01:11:18 +0200

    CHA-114 heroku.yml changes

--- IOProject/settings.py
+++ IOProject/settings.py
@@ -100,8 +100,10 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(
-    default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    default=DATABASE_URL)
 DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(dj_database_url.config(
+    conn_max_age=500, ssl_require=True))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
